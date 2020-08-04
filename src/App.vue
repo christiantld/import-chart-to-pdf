7adcc4072ba2fbcc81768309ad153920afe96f35
Christian Toledo <christian.odt@gmail.com>
2020-08-04 16:52:04 -0300

    testing charts

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
+//STEP 4: Render the chart
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="chart-container">
+      <fusioncharts
+        :type="type"
+        :width="width"
+        :height="height"
+        :dataformat="dataFormat"
+        :dataSource="dataSource"
+      ></fusioncharts>
+      <fusioncharts
+        :type="type"
+        :width="width"
+        :height="height"
+        :dataformat="dataFormat"
+        :dataSource="dataSource"
+      ></fusioncharts>
+      <fusioncharts
+        :type="type"
+        :width="width"
+        :height="height"
+        :dataformat="dataFormat"
+        :dataSource="dataSource"
+      ></fusioncharts>
+    </div>
   </div>
 </template>
-
+// STEP 1: to include the dependencies
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue";
+import VueFusionCharts from "vue-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+Vue.use(VueFusionCharts, FusionCharts, Column2D, FusionTheme);
+
+// STEP 2: Prepare the data
+const chartColumnData = require("./resources/chartColumnData.json");
+// STEP 3: Configure your chart
+const dataSource = {
+  chart: {
+    caption: "Countries With Most Oil Reserves [2017-18]",
+    subcaption: "In MMbbl = One Million barrels",
+    xaxisname: "Country",
+    yaxisname: "Reserves (MMbbl)",
+    numbersuffix: "K",
+    theme: "fusion",
+    crossLineAnimation: "0",
+    exportEnabled: 1,
+  },
+  data: chartColumnData,
+};
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      type: "column2d",
+      renderAt: "chart-container",
+      width: "550",
+      height: "350",
+      dataFormat: "json",
+      dataSource: dataSource,
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
